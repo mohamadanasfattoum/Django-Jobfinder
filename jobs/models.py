24a89ad573f6f6c814f17d2548dc9bbe85d094b2
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-11 21:42:22 +0200

    add class Categorie and translation in models

--- jobs/models.py
+++ jobs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 TYPY_CHOICES =(
     ('full','full'),
@@ -7,19 +8,26 @@ TYPY_CHOICES =(
     ('freelance','freelance'),
 )
 class Job(models.Model):
-    name = models.CharField(max_length=120)
-    image = models.ImageField(upload_to='job')
-    logo = models.ImageField(upload_to='logo')
-    salary= models.FloatField()
-    type = models.CharField(max_length=10, choices=TYPY_CHOICES)
-    company = models.ForeignKey('Company', related_name= 'company_jop', on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(_('Name'), max_length=120)
+    image = models.ImageField(_('Image'), upload_to='job')
+    logo = models.ImageField(_('Logo'), upload_to='logo')
+    salary= models.FloatField(_('Salary'))
+    education =models.CharField(_('Education'), max_length=500, null=True, blank=True )
+    experience =models.CharField(_('Experience'), max_length=500, null=True, blank=True)
+    type = models.CharField(_('Type'), max_length=10, choices=TYPY_CHOICES)
+    company = models.ForeignKey('Company', verbose_name=_('Company'), related_name= 'company_jop', on_delete=models.SET_NULL, null=True, blank=True)
 
 class Company(models.Model):
-    name = models.CharField(max_length=120)
-    logo = models.ImageField(upload_to='logo')
-    salary= models.FloatField()
-    location = models.CharField(max_length=120)
-    description = models.TextField(max_length=5000)
-    web = models.CharField(max_length=120)
-    email = models.TextField(max_length=500)
+    name = models.CharField(_('Name'), max_length=120)
+    logo = models.ImageField(_('Logo'), upload_to='logo')
+    salary= models.FloatField(_('Salary') )
+    location = models.CharField(_('Location'), max_length=120)
+    description = models.TextField(_('Description'), max_length=5000)
+    web = models.CharField(_('Web'), max_length=120)
+    email = models.TextField(_('Email'), max_length=500)
 
+class Categorie(models.Model):
+    name = models.CharField(_('Name'), max_length=120)
+    job = models.ForeignKey(Job, related_name='categorie_job', on_delete=models.CASCADE ) 
+    logo = models.ImageField(_('Logo'), upload_to='logo')
+    rate = models.IntegerField(_('Rate'))
