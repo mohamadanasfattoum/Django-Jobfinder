e5e3fe8ad0ca0e736ec1ccea1e8df12aad027843
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-13 20:08:09 +0200

    add job_detail.html ,job_list.html ,base.html

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'taggit',
+
+
+    # my Apps
     'jobs',
 ]
 
@@ -55,7 +59,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
