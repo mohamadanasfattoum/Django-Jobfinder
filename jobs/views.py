266ac4f5bfd2a5189d0a51c5defd624f910c0961
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-13 21:42:36 +0200

    git push

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Job, Company, Categorie
 
-
+class Job_list(generic.ListView):
+    model=Job
--- project/urls.py
+++ project/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from jobs.views import Job_list
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path('jobs/', Job_list.as_view()),
 ]
 
 
