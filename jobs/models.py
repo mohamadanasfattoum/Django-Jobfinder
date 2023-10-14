11bd1844b0d7a180bb24070aa68f42d64b166247
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-14 19:08:33 +0200

    add coreacted models

--- jobs/models.py
+++ jobs/models.py
@@ -8,22 +8,22 @@ TYPY_CHOICES =(
     ('remote','remote'),
     ('freelance','freelance'),
 )
+
 class Job(models.Model):
     name = models.CharField(_('Name'), max_length=120)
     image = models.ImageField(_('Image'), upload_to='job')
-    logo = models.ImageField(_('Logo'), upload_to='logo')
     job_description = models.TextField(_('Job_description'), max_length=5000, null=True, blank=True)
     rounding_salary= models.CharField(_('Rounding_salary'),max_length=50, null=True, blank=True)
     education =models.CharField(_('Education'), max_length=500, null=True, blank=True )
-    experience =models.CharField(_('Experience'), max_length=500, null=True, blank=True)
+    experience =models.TextField(_('Experience'), max_length=5000, null=True, blank=True)
     type = models.CharField(_('Type'), max_length=10, choices=TYPY_CHOICES)
-    company = models.ForeignKey('Company', verbose_name=_('Company'), related_name= 'company_jop', on_delete=models.SET_NULL, null=True, blank=True)
-
+    company = models.ForeignKey('Company', verbose_name=_('Company'), related_name= 'company_jop', on_delete=models.CASCADE )
+    salary= models.CharField(_('Salary'),max_length=50, null=True, blank=True )
+    categorie = models.ForeignKey('Categorie', verbose_name=_('Categorie'), related_name='categorie_job', on_delete=models.CASCADE,max_length=5000, null=True, blank=True )
 
 class Company(models.Model):
     name = models.CharField(_('Name'), max_length=120)
     logo = models.ImageField(_('Logo'), upload_to='logo')
-    salary= models.CharField(_('Salary'),max_length=50, null=True, blank=True )
     location = models.CharField(_('Location'), max_length=120)
     company_description = models.TextField(_('Company_description'), max_length=5000, null=True, blank=True)
     web = models.CharField(_('Web'), max_length=120)
@@ -33,6 +33,4 @@ class Company(models.Model):
 
 class Categorie(models.Model):
     name = models.CharField(_('Name'), max_length=120)
-    job = models.ForeignKey(Job, related_name='categorie_job', on_delete=models.CASCADE ) 
     logo = models.ImageField(_('Logo'), upload_to='logo')
-    count = models.IntegerField(_('count'), null=True, blank=True)
