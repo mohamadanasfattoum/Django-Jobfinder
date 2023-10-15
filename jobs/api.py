24d57b07c7bfe429f66543e0267a2447cca4b212
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-15 20:12:54 +0200

    add IsAuthenticated, SAFE_METHODS

--- jobs/api.py
+++ jobs/api.py
@@ -1,18 +1,28 @@
 from rest_framework import generics
 from rest_framework.decorators import api_view
+from rest_framework.permissions import BasePermission #, IsAuthenticated, SAFE_METHODS
 from rest_framework.response import Response
+
 
 
 
 from .serializers import JobListSerializer, JobDetailSerializer
 from .models import Job
 
+# class ReadOnly(BasePermission):
+#     def has_permission(self, request, view):
+#         return request.method in SAFE_METHODS
 
 class JobListApi(generics.ListCreateAPIView):
+    #permission_classes = [IsAuthenticated]
     serializer_class = JobListSerializer
     queryset = Job.objects.all()
 
-
+# def get(self, request, format=None):
+#         content = {
+#             'status': 'request was permitted'
+#         }
+#         return Response(content)
 
 
 class JobDetailApi(generics.RetrieveUpdateDestroyAPIView):
--- project/settings.py
+++ project/settings.py
@@ -133,3 +133,15 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
+    ]
+
+
+}
+
+
